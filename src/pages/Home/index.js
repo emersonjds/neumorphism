dd5adb1599edd0fcad7f1b541eff4d837037862a
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-13 16:14:36 -0300

    config shadow effects

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,68 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
-import {Container, SafeArea, ContainerSpacing, TopContainer} from './styles';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {
+  Container,
+  SafeArea,
+  ContainerSpacing,
+  TopContainer,
+  TopShadow,
+  BottomShadow,
+} from './styles';
+import Icon from 'react-native-vector-icons/AntDesign';
+Icon.loadFont();
 
-export default class Home extends Component {
-  render() {
+const gray = '#91A18D';
+
+export default function Home() {
+  const NeuMorph = ({children, size, style}) => {
     return (
-      <Container>
-        <SafeArea>
-          <ContainerSpacing>
-            <TopContainer>
-              <Text>Emerson</Text>
-            </TopContainer>
-          </ContainerSpacing>
-        </SafeArea>
-      </Container>
+      <TopShadow>
+        <BottomShadow>
+          <View
+            style={[
+              styles.inner,
+              {
+                width: size || 40,
+                height: size || 40,
+                borderRadius: size / 2 || 40 / 2,
+              },
+              style,
+            ]}>
+            {children}
+          </View>
+        </BottomShadow>
+      </TopShadow>
     );
-  }
+  };
+
+  return (
+    <Container>
+      <SafeArea>
+        <ContainerSpacing>
+          <TopContainer>
+            <NeuMorph>
+              <Icon name="arrowleft" size={20} color={gray} />
+            </NeuMorph>
+
+            <View>
+              <Text> PLAYING NOW</Text>
+            </View>
+            <NeuMorph>
+              <Icon name="bars" size={20} color={gray} />
+            </NeuMorph>
+          </TopContainer>
+        </ContainerSpacing>
+      </SafeArea>
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  inner: {
+    backgroundColor: '#DEE9f7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#E2ECFD',
+    borderWidth: 1,
+  },
+});
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  background-color: #fff;
+  background-color: #dee9fd;
   align-items: center;
-  justify-content: center;
   height: 100%;
 `;
 export const SafeArea = styled.SafeAreaView`
@@ -12,4 +11,19 @@ export const SafeArea = styled.SafeAreaView`
 export const ContainerSpacing = styled.View`
   margin: 32px;
 `;
-export const TopContainer = styled.View``;
+export const TopContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TopShadow = styled.View`
+  shadow-offset: {
+    width: -6px;
+    height: -6px;
+  }
+  shadow-opacity: 1px;
+  shadow-radius: 6px;
+  shadow-color: #fbffff;
+`;
+export const BottomShadow = styled.View``;
